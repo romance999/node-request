8961328c5fd276bba9dacfbcfe88cbaad4d9ddee
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-04-04 16:31:45 -0400

    Add files via upload

--- request.js
+++ request.js
@@ -11,5 +11,6 @@ function run()
         console.log(json.body) 
         //we do json.body to print the jsons body content, you can also do just json but it will show everything
         //to specify data inside the body, for example "version", you would do json.body.version. keep in mind its case sensitive 
+        console.log(json.body.version);
     })
 }
